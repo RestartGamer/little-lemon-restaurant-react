ae65cd513149c193cae3e80862e367a53b397062
RestartGamer <ckorkmaz56@gmail.com>
2026-04-21 17:57:12 +0100

    feat(app): implement initial structure of typography

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,37 @@
 import { useState } from 'react'
 import './App.css'
 
+const typography = {
+  fontFamily: `"Karla", sans-serif`,
+
+  heroTitle: {
+    fontFamily: `"Markazi Text", sans-serif`,
+    fontWeight: 500, //Medium
+    fontSize: "60px",
+  },
+  headingTitle: {
+    fontFamily: `"Markazi Text", sans-serif`,
+    fontWeight: 400, //Medium
+    fontSize: "45px",
+  },
+  sectionTitle: {
+
+  },
+  cardTitle: {
+
+  },
+  bodyLarge: {
+
+  },
+  bodyMedium: {
+
+  },
+  bodySmall: {
+
+  },
+
+}
+
 function App() {
   
   return ()
