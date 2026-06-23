6484560241c513c948e5d68e12edf54f4f496942
RestartGamer <ckorkmaz56@gmail.com>
2026-06-23 15:15:46 +0100

    refactor(slideshow): rewrite and reorganize the slidesbar/slideshow

--- client/src/sections/SlideShowSection.jsx
+++ client/src/sections/SlideShowSection.jsx
@@ -1,53 +1,81 @@
-import { Box, Stack, Typography, ButtonBase } from "@mui/material"
-import { SlideShowItem } from "../components"
-import { foodItems } from "../../../server/data/foodItems"
-import { convert } from "../utils/muiConverter"
-
+import { Box, Stack } from "@mui/material";
+import { SlideShowItem } from "../components";
+import { foodItems } from "../../../server/data/foodItems";
 
 export function SlideShowSection() {
     return (
-        <Box className="SlideShowSection__Container SlideShowSection__Clipper" sx={{
-            width: "100%",
-            overflow: "clip",
-            "--precalculated-width": { md: "500px", xs: "400px" },
-            "--item-width": { md: "500px", xs: "400px" },
-            marginInline: "var(--lateral-margin)",
-            display: "flex",
-            justifyContent: "center",
-        }}>
-            <Box className="SlideShowSection__Content SlideShowSection__Scroller" sx={{
-                overflowX: "auto",
-                width: "fit-content",
-            }}>
-                <Stack className="SlideShowSection__ScrollContent" direction="row" sx={{
-                    justifyContent: "flex-start",
-                    alignItems: "center",
-                    width: "fit-content",
-                    height: "fit-content",
-                    gap: "calc((100vw - var(--lateral-margin)) - var(--item-width))"
-                }}>
+        <Box
+            className="SlideShowSection__Container SlideShowSection__Clipper"
+            sx={{
+                "--container-lateral-padding": {
+                    xs: "30px",
+                    md: "60px",
+                    lg: "80px",
+                    xl: "300px",
+                },
+                "--gap": "10px",
+                "--visible-items": {
+                    xs: 1,
+                    md: 2,
+                    lg: 3,
+                    xl: 4,
+                },
+                "--frame-width":
+                    "calc((100vw - var(--container-lateral-padding) * 2 - (var(--gap) * (var(--visible-items) - 1))) / var(--visible-items))",
+
+                width: "100%",
+                overflow: "clip",
+                px: "var(--container-lateral-padding)",
+                boxSizing: "border-box",
+            }}
+        >
+            <Box
+                className="SlideShowSection__Content SlideShowSection__Scroller"
+                sx={{
+                    width: "100%",
+                    overflowX: "auto",
+                    scrollSnapType: "x mandatory",
+                }}
+            >
+                <Stack
+                    className="SlideShowSection__ScrollContent"
+                    direction="row"
+                    sx={{
+                        width: "max-content",
+                        height: "fit-content",
+                        alignItems: "center",
+                        gap: "var(--gap)",
+                    }}
+                >
                     {foodItems.map((foodItem) => {
                         const { id, title, price, src } = foodItem;
 
                         return (
-                            <SlideShowItem
+                            <Box
                                 key={id}
-                                item={foodItem}
-                                title={title}
-                                price={price}
-                                src={src}
+                                className="SlideShowSection__ItemFrame"
+                                sx={{
+                                    flex: "0 0 var(--frame-width)",
+                                    width: "var(--frame-width)",
+                                    scrollSnapAlign: "start",
+
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    //outline: "1px solid red",
+                                }}
                             >
-                                <ButtonBase>
-                                    <Typography>
-                                        Add to cart
-                                    </Typography>
-                                </ButtonBase>
-                            </SlideShowItem>
+                                <SlideShowItem
+                                    item={foodItem}
+                                    title={title}
+                                    price={price}
+                                    src={src}
+                                />
+                            </Box>
                         );
                     })}
                 </Stack>
             </Box>
-
         </Box>
-    )
+    );
 }
